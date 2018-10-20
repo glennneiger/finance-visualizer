000803208caa718576881112c67dfa76ce2f837b
drichmond3 <darrien.richmond@gmail.com>
2018-10-20 17:09:00 -0500

    Remove babel loader meaning browsers must support ES6 to use this application.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,2 @@
-import '../styles/index.scss';
-
-console.log('webpack starterkit');
+console.log('Hello World! I\'m here!!!');
+// 
--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -32,11 +32,6 @@ module.exports = merge(common, {
         }
       },
       {
-        test: /\.(js)$/,
-        include: Path.resolve(__dirname, '../src'),
-        loader: 'babel-loader'
-      },
-      {
         test: /\.s?css$/i,
         use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
       }
